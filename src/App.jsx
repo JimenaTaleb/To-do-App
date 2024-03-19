d4704d0a11fdeba3892e5c294c572b971e0924d9
Jimena Taleb <jime-taleb@hotmail.com>
2024-03-19 17:55:29 -0300

    adding app styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import Header from './components/Header';
-import Container from './components/Container';
-import Footer from './components/Footer';
-import backgroundImage from './assets/background.jpg'
+import Header from "./components/Header";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
+import backgroundImage from "./assets/background.jpg";
+import Box from "@mui/material/Box";
 
 function App() {
   const divStyle = {
     backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    minHeight: '100vh',
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    minHeight: "100vh",
     margin: 0,
     padding: 0,
   };
 
   return (
-    <div style={divStyle}>
-      <Header />
-      <Container />
-      <Footer />
-    </div>
+      <Box display="flex" flexDirection="column" justifyContent="space-between" alignItems="center" height="100%" style={divStyle}>
+        <Header />
+        <Container />
+        <Footer />
+      </Box>
   );
 }
 
