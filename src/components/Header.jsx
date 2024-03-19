5fd72341eb7e505a867ae887aef70aee58520a8b
Jimena Taleb <jime-taleb@hotmail.com>
2024-03-19 20:32:10 -0300

    importing roboto font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,27 @@
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 
 export default function Header() {
   return (
     <header>
-      <Typography
-        align="center"
-        sx={{
-          fontSize: { xs: 30, md: 40, lg: 50 },
-        }}
-      >
-        To do App by Ji
-      </Typography>
+      <Box pt={5}>
+        <Typography
+          align="center"
+          sx={{
+            fontSize: { xs: 30, md: 40, lg: 50 },
+          }}
+        >
+          To do App
+        </Typography>
+        <Typography
+          align="right"
+          sx={{
+            fontSize: { xs: 15, md: 25, lg: 35 },
+          }}
+        >
+          by Ji
+        </Typography>
+      </Box>
     </header>
   );
 }
+
