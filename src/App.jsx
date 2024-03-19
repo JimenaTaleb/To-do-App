fc2c1943f8bb4e441786154152a993c483592d85
Jimena Taleb <jime-taleb@hotmail.com>
2024-03-19 17:30:56 -0300

    adding background styles to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import Header from './components/Header'
-import Container from './components/Container'
-import Footer from './components/Footer'
+import Header from './components/Header';
+import Container from './components/Container';
+import Footer from './components/Footer';
+import backgroundImage from './assets/background.jpg'
 
 function App() {
+  const divStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    minHeight: '100vh',
+    margin: 0,
+    padding: 0,
+  };
 
   return (
-    <div>
-      <Header/>
-      <Container/>
-      <Footer/>
+    <div style={divStyle}>
+      <Header />
+      <Container />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
